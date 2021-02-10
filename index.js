d2edd459a8b7db099075ef71e7e3f0de658dbffc
Pauli Niva <pauli.niva@helsinki.fi>
2021-02-10 23:21:21 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,120 +1,119 @@
 require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const app = express()
 const Person = require('./models/person')
-const { response } = require('express')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
-        .then(persons => {
-            res.json(persons)
+  Person.find({})
+    .then(persons => {
+      res.json(persons)
     })
     .catch(error => next(error))
-  })
+})
 
-app.get('/info', (req, res) => {
-    Person.countDocuments()
-        .then((count) => {
-            res.send(`<p>Phonebook has info for ${count} people</p>${Date()}`)
-        })
-        .catch(error => next(error))
+app.get('/info', (req, res, next) => {
+  Person.countDocuments()
+    .then((count) => {
+      res.send(`<p>Phonebook has info for ${count} people</p>${Date()}`)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if (!body.name) {
-        return res.status(400)
-            .send({ error: 'Name cannot be empty' })
-        }
+  if (!body.name) {
+    return res.status(400)
+      .send({ error: 'Name cannot be empty' })
+  }
 
-    if (!body.number) {
-        return res.status(400)
-            .send({ error: 'Phonenumber cannot be empty' })
-    }
+  if (!body.number) {
+    return res.status(400)
+      .send({ error: 'Phonenumber cannot be empty' })
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
     .catch((error) => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if (!body.name) {
-        return res.status(400)
-            .send({ error: 'Name cannot be empty' })
-        }
+  if (!body.name) {
+    return res.status(400)
+      .send({ error: 'Name cannot be empty' })
+  }
 
-    if (!body.number) {
-        return res.status(400)
-            .send({ error: 'Phonenumber cannot be empty' })
-    }
+  if (!body.number) {
+    return res.status(400)
+      .send({ error: 'Phonenumber cannot be empty' })
+  }
 
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: body.number })
-      .then(updatedPerson => {
-        res.json(updatedPerson)
-      })
-      .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: body.number })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
-  }
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
 // olemattomien osoitteiden käsittely
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-      return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name ==='ValidationError') {
-        return res.status(400).json({ error: error.message })
-      }
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name ==='ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 // tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
@@ -122,5 +121,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
